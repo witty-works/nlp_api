c50b14128f9500dcd9a71cef8630a1b2511689ee
lnazarenko <oknerazan@gmail.com>
2021-08-25 13:30:14 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -110,7 +110,7 @@ async def check_query(user_request_in: UserRequestIn):
     matcher.add("TerminologyList", patterns)
     
     #functions for German rules& false positives
-    if lang.local == "de":
+    if lang.local == "de_DE":
         #Male coded words and related false positives catch
         list_male_coded= MaleCodedWordAnalysis(lang, tokens)
 
@@ -128,7 +128,7 @@ async def check_query(user_request_in: UserRequestIn):
         # full list
         list_full = list_male_coded+list_empty_words+list_gender_denom+list_boast + list_discrim
     #function for English rules
-    elif lang.local == "en":
+    elif lang.local == "en_GB":
 
         list_male_coded_en= RulesBasedEN(tokens, df_male_coded_words, "MaleCodedWords-English", "male_coded_terms")
         list_full = list_male_coded_en
