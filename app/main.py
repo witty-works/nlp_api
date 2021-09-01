44282ee2e8c46bd75b30f40b7c08c87cfba920e6
Sümer Cip <sumer.cip@platform.sh>
2021-09-01 16:20:22 +0300

    Debug Config issue

--- app/main.py
+++ app/main.py
@@ -85,9 +85,8 @@ app.add_middleware(
 from app.middleware import BlackfireFastAPIMiddleware
 
 config = Config()
-print(config.variable("BLACKFIRE_ENABLED"), config.is_valid_platform())
-if config.is_valid_platform() and config.variable("BLACKFIRE_ENABLED") == True:
-    app.add_middleware(BlackfireFastAPIMiddleware)
+#if config.is_valid_platform() and config.variable("BLACKFIRE_ENABLED") == True:
+app.add_middleware(BlackfireFastAPIMiddleware)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
