fccaf9f52eac12f27afa57f4ba0b95b91374fb3d
Lukas Kahwe Smith <smith@pooteeweet.org>
2021-09-10 14:04:10 +0200

    add logging

--- app/models.py
+++ app/models.py
@@ -7,6 +7,31 @@ class UserRequestIn(BaseModel):
     lang: Optional[str] = "auto"
     fallback_lang: Optional[str] = "de"
     response_lang: Optional[str] = "de_DE"
+    id: Optional[str] = "anon"
+
+    def toDict(self):
+        return {
+            "text": self.text,
+            "lang": self.lang,
+            "fallback_lang": self.fallback_lang,
+            "response_lang": self.response_lang,
+        }
+
+class UserRequestInEvent(UserRequestIn):
+    alternative: str
+    start: int
+    end: int
+
+    def toDict(self):
+        return {
+            "text": self.text,
+            "lang": self.lang,
+            "fallback_lang": self.fallback_lang,
+            "response_lang": self.response_lang,
+            "alternative": self.alternative,
+            "start": self.start,
+            "end": self.end,
+        }
 
 class ResultOut(BaseModel):
     start: int
@@ -40,6 +65,33 @@ class ResultOut(BaseModel):
         object.__setattr__(self, 'reason', reason)
         object.__setattr__(self, 'solution', solution)
 
+    def toDict(self):
+        return {
+            "start": self.start,
+            "end": self.end,
+            "category": self.category,
+            "text": self.text,
+            "label": self.label,
+            "reason": self.reason,
+            "solution": self.solution,
+            "alternatives": self.alternatives,
+        }
+
 class ResultsOut(BaseModel):
     results: List[ResultOut]
     language: str
+
+    def factory(results, lang):
+        return ResultsOut(results, lang.locale)
+
+    factory = staticmethod(factory)
+
+    def __init__(self, results, language):
+        object.__setattr__(self, 'results', results)
+        object.__setattr__(self, 'language', language)
+
+    def toDict(self):
+        return {
+            "results": [result.toDict() for result in self.results],
+            "language": self.language,
+        }
