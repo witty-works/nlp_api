ce32c1d649c169adf31bb819dfc2009a988d04f9
lnazarenko <oknerazan@gmail.com>
2021-09-13 16:35:20 +0200

    updated test output correspondently to the current output

--- tests/test_api.py
+++ tests/test_api.py
@@ -35,7 +35,7 @@ def test_api():
             "category": "boasting_words",
             "start": 11,
             "end": 16,
-            "alternatives": [],
+            "alternatives": ["-"],
             "label": "Superlative Wörter",
             "reason": "Mit diesem Begriff nutzen Sie die Sprache der Superlative - oder auch Prahlerei - genannt. Superlative Begriffe in der Beschreibung der Firma schrecken viele Bewerber:innen ab, da bei ihnen unbewusst die Empfindung entsteht, im Sinne der Superlative mithalten und so sein zu müssen.",
             "solution": "Es wird dringend empfohlen, solche Begriffe zu vermeiden."
@@ -45,7 +45,7 @@ def test_api():
             "category": "boasting_words",
             "start": 17,
             "end": 25,
-            "alternatives": [],
+            "alternatives": ["-"],
             "label": "Superlative Wörter",
             "reason": "Mit diesem Begriff nutzen Sie die Sprache der Superlative - oder auch Prahlerei - genannt. Superlative Begriffe in der Beschreibung der Firma schrecken viele Bewerber:innen ab, da bei ihnen unbewusst die Empfindung entsteht, im Sinne der Superlative mithalten und so sein zu müssen.",
             "solution": "Es wird dringend empfohlen, solche Begriffe zu vermeiden."
@@ -97,7 +97,7 @@ def test_api_response_lang():
             "category": "boasting_words",
             "start": 11,
             "end": 16,
-            "alternatives": [],
+            "alternatives": ["-"],
             "label": "Boasting terms",
             "reason": "With this term you make use of superlative - or also called  boasting - language. Superlative terms in the description of a company discourage many applicants, since because such language unconsciously provokes the feeling within candidates that they will have to comply with and be like the superlatives.",
             "solution": "It is highly recommended to avoid such terms."
@@ -107,7 +107,7 @@ def test_api_response_lang():
             "category": "boasting_words",
             "start": 17,
             "end": 25,
-            "alternatives": [],
+            "alternatives": ["-"],
             "label": "Boasting terms",
             "reason": "With this term you make use of superlative - or also called  boasting - language. Superlative terms in the description of a company discourage many applicants, since because such language unconsciously provokes the feeling within candidates that they will have to comply with and be like the superlatives.",
             "solution": "It is highly recommended to avoid such terms."
