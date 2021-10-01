561103c392daa69cf6b2bbac8b7a8492a666ce03
Lukas Kahwe Smith <smith@pooteeweet.org>
2021-10-01 11:03:36 +0200

    disable SEHR_GEEHRTER_NAME rule fixes #107

--- app/main.py
+++ app/main.py
@@ -229,11 +229,18 @@ async def languagetools(lang, text):
             "language": lang.locale,
             "disabledRules": "DE_CASE"
         }
+        rule_ids_ignore = [
+            "SEHR_GEEHRTER_NAME"
+        ]
+
         async with session.post(url + "/check", data=payload) as r:
             result = await r.json()
 
             if "matches" in result:
                 for match in result["matches"]:
+                    if match["rule"]["id"] in rule_ids_ignore:
+                        continue
+
                     offset = int(match["offset"])
                     end = offset + int(match["length"])
                     alternatives = []
