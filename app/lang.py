f342603d02d97fbcd887c2ca2c21869c1c2a4ef1
Lukas Kahwe Smith <smith@pooteeweet.org>
2021-10-10 18:39:51 +0200

    cleanups

--- app/lang.py
+++ app/lang.py
@@ -1,11 +1,15 @@
 import gettext
 
-from app.models import UserRequestIn
-
 class Lang(object):
     def __init__(self, locale):
         self.locale = locale[0:2]
 
-        language = gettext.translation("messages", localedir="locales", languages=[locale.replace("-", "_")])
+        language = gettext.translation(
+            "messages",
+            localedir="locales",
+            languages=[locale.replace("-", "_")]
+        )
+
         language.install()
+
         self._ = language.gettext
