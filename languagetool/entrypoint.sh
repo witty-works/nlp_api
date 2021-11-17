4239b62f88bc0f74ebefe8a2dc5d6fe58cb3ce57
Sebastian Hutter <seh@foryouandyourcustomers.com>
2021-11-17 16:22:41 +0100

    Add dockerfile for language server

---
+++ languagetool/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# calculate 80% of available memory for java heap size
+avail_mem=$(free -m | awk '/Mem:/ {print $2}')
+heap=$(echo -n "${avail_mem}/100*80" | bc)
+
+java -noverify \
+    -Xms${heap}M \
+    -Xmx${heap}M \
+    -XX:+UseG1GC \
+    -XX:+UseStringDeduplication \
+    -cp languagetool-server.jar org.languagetool.server.HTTPServer \
+    --public \
+    --config server.properties \
+    --port $PORT \
+    --allow-origin "*"
