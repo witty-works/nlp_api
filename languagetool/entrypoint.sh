d65bc93475dc410dc9077e633fe9e31b05829c0f
Sebastian Hutter <seh@foryouandyourcustomers.com>
2021-12-04 09:30:26 +0100

    Add sshd to language tool container
    I am moving to app service per container instance.
    As multicontainer setup is just not stable

--- languagetool/entrypoint.sh
+++ languagetool/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# start sshd
+/usr/sbin/sshd
+
 # calculate 80% of available memory for java heap size
 avail_mem=$(free -m | awk '/Mem:/ {print $2}')
 heap=$(echo -n "${avail_mem}/100*${HEAP_PERCENTAGE}" | bc)
@@ -10,7 +13,7 @@ echo "Found available memory: ${avail_mem}"
 echo "Calculated heap size (by percentage ${HEAP_PERCENTAGE}): ${heap}" 
 echo ""
 
-java -noverify \
+su-exec languagetool java -noverify \
     -Xms${heap}M \
     -Xmx${heap}M \
     -XX:+UseG1GC \
