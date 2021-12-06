0092243370a6b6d2c1d92e4cf86e1c448eeb508a
Karolina Bozek <bozek.karolina@witty.works>
2021-12-06 22:09:28 +0100

    Add more snapshots for other tests.

--- tests/convert.py
+++ tests/convert.py
@@ -1,9 +1,54 @@
 import json
 
-list = {
-    "text": "Wir suchen Ninja Programmierer für unsere Kunden",
-    "config": {"store_context": False},
-}
+list = [
+    {
+        "text": "Manager",
+        "context": "",
+        "category": "gendered",
+        "subcategory": "function",
+        "start": 17,
+        "end": 24,
+        "alternatives": ["Management", "Manager:in"],
+        "label": "Geschlechtsspezifisch: Spezielle Funktionen",
+        "reason": "Aus wirtschaftlich-historischen Gründen wird unbewusst ein Bild eines Mannes vor dem inneren Auge hervorgerufen. Das weibliche Geschlecht oder andere Geschlechtsidentitäten werden nicht sichtbar. Und sie fühlen sich nicht zugehörig.",
+        "solution": "Umgehen Sie mit anderen Worten das unbewusst hervorgerufene Bild. Nutzen Sie eher das Nomen, das die Tätigkeit bezeichnet, um das unbewusste Bild zu umgehen. Oder verwenden Sie eine geschlechtsneutrale Bezeichnung.",
+    },
+    {
+        "text": "der",
+        "context": "",
+        "category": "gendered",
+        "subcategory": "function",
+        "start": 13,
+        "end": 16,
+        "alternatives": ["der:die"],
+        "label": "Geschlechtsspezifisch: Spezielle Funktionen",
+        "reason": "Aus wirtschaftlich-historischen Gründen wird unbewusst ein Bild eines Mannes vor dem inneren Auge hervorgerufen. Das weibliche Geschlecht oder andere Geschlechtsidentitäten werden nicht sichtbar. Und sie fühlen sich nicht zugehörig.",
+        "solution": "Umgehen Sie mit anderen Worten das unbewusst hervorgerufene Bild. Nutzen Sie eher das Nomen, das die Tätigkeit bezeichnet, um das unbewusste Bild zu umgehen. Oder verwenden Sie eine geschlechtsneutrale Bezeichnung.",
+    },
+    {
+        "text": "Boss",
+        "context": "",
+        "category": "gendered",
+        "subcategory": "leadership",
+        "start": 34,
+        "end": 38,
+        "alternatives": [
+            "Leitende Kraft",
+            "Leitungsperson",
+            "Leitung",
+            "Vorgesetzte:r",
+            "Leitende Kräfte",
+            "Leitungskräfte",
+            "Leitungspersonen",
+            "Vorgesetzte",
+            "Entscheidungsbefugte Person",
+            "Verantwortliche Person",
+        ],
+        "label": "Geschlechtsspezifisch: Führungsstereotyp",
+        "reason": "Besetzt das Thema der Führung stereotyp männlich und traditionell hierarchisch.",
+        "solution": "Verwenden Sie Begriffe, die von einer unterstützenden Vorstellung der Führung ausgeht und verschiedene Geschlechter meinen kann.",
+    },
+]
 
 conv = json.dumps(list)
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,6 @@ import json
 import os
 
 import pytest
-import yaml
 from pathlib import Path
 
 from fastapi.applications import FastAPI
@@ -12,12 +11,18 @@ from app.main import redis, set_rules
 import json
 from app.models import RequestIn
 
+
 client = TestClient(app)
 
 
-def json_to_yaml(json_string):
-    obj = json.loads(json_string)
-    return yaml.dump(obj, indent=2)
+def test_read_main():
+    response = client.get("/", allow_redirects=False)
+    assert response.status_code == 301
+
+
+def test_read_form():
+    response = client.get("/form")
+    assert response.status_code == 200
 
 
 @pytest.mark.parametrize(
@@ -25,19 +30,125 @@ def json_to_yaml(json_string):
     list(Path("tests/test_cases").iterdir()),
 )
 def test_json(case_dir, snapshot):
-    # Read input files from the case directory.
-    print("LALALALAL")
-    input_json = case_dir.joinpath("input.json").read_text()
-    print("**********")
-    print(input_json)
-    print("*********")
-    # Call the tested function.
-    response = client.post("/check", json=input_json)
-    output = response.json
-    print(type(str(output)))
-    output_yaml = json_to_yaml(input_json)
-    print(type(output_yaml))
-    # Snapshot the return value.
 
+    # Read input files from the case directory.
+    input_json = case_dir.joinpath("input.json").read_text()
+    # Call the tested endpoint.
+    response = client.post("/check", json=json.loads(input_json))
+    assert response.status_code == 200
+    # output must be string
+    output = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
+    # Snapshot the return value.
     snapshot.snapshot_dir = case_dir
-    snapshot.assert_match(output_yaml, "output.yaml")
+    snapshot.assert_match(output, "output.json")
+
+
+@pytest.mark.parametrize(
+    "case_dir",
+    list(Path("tests/test_orthography").iterdir()),
+)
+def test_orthoraphy(case_dir, snapshot):
+
+    # Read input files from the case directory.
+    input_json = case_dir.joinpath("input.json").read_text()
+    # Call the tested endpoint.
+    response = client.post("/check", json=json.loads(input_json))
+    assert response.status_code == 200
+    # output must be string
+    output = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
+    # Snapshot the return value.
+    snapshot.snapshot_dir = case_dir
+    snapshot.assert_match(output, "output.json")
+
+
+@pytest.mark.parametrize(
+    "case_dir",
+    list(Path("tests/test_gender_ending").iterdir()),
+)
+def test_gender_ending(case_dir, snapshot):
+
+    # Read input files from the case directory.
+    input_json = case_dir.joinpath("input.json").read_text()
+    # Call the tested endpoint.
+    response = client.post("/check", json=json.loads(input_json))
+    assert response.status_code == 200
+    # output must be string
+    output = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
+    # Snapshot the return value.
+    snapshot.snapshot_dir = case_dir
+    snapshot.assert_match(output, "output.json")
+
+
+def test_api_missing_data():
+    response = client.post("/check")
+    assert response.status_code == 422
+
+
+def test_api_empty_data():
+    request_data = {}
+
+    response = client.post("/check", json=request_data)
+    assert response.status_code == 422
+
+
+@pytest.mark.parametrize(
+    "case_dir",
+    list(Path("tests/test_gender_ending").iterdir()),
+)
+def test_gender_ending(case_dir, snapshot):
+
+    # Read input files from the case directory.
+    input_json = case_dir.joinpath("input.json").read_text()
+    # Call the tested endpoint.
+    response = client.post("/check", json=json.loads(input_json))
+    assert response.status_code == 200
+    # output must be string
+    output = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
+    # Snapshot the return value.
+    snapshot.snapshot_dir = case_dir
+    snapshot.assert_match(output, "output.json")
+
+
+@pytest.mark.parametrize(
+    "case_dir",
+    list(Path("tests/test_language_detection").iterdir()),
+)
+def test_language_detection(case_dir, snapshot):
+
+    # Read input files from the case directory.
+    input_json = case_dir.joinpath("input.json").read_text()
+    # Call the tested endpoint.
+    response = client.post("/check", json=json.loads(input_json))
+    assert response.status_code == 200
+    # output must be string
+    output = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
+    # Snapshot the return value.
+    snapshot.snapshot_dir = case_dir
+    snapshot.assert_match(output, "output.json")
+
+
+@pytest.mark.parametrize(
+    "case_dir",
+    list(Path("tests/test_language_detection_fail").iterdir()),
+)
+def test_language_detection_fail(case_dir, snapshot):
+
+    # Read input files from the case directory.
+    input_json = case_dir.joinpath("input.json").read_text()
+    # Call the tested endpoint.
+    response = client.post("/check", json=json.loads(input_json))
+    assert response.status_code == 422
+
+
+def test_log(snapshot):
+    # Read input files from the case directory.
+    case_dir = Path("tests/test_log")
+    input_json = case_dir.joinpath("input.json").read_text()
+    print("input_json", input_json)
+    # Call the tested endpoint.
+    response = client.post("/log", json=json.loads(input_json))
+    assert response.status_code == 201
+    # output must be string
+    output = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
+    snapshot.snapshot_dir = "tests/test_log"
+    snapshot.assert_match(output, "output.json")
