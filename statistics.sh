e6e27ec339a363d9be69d978424bebca7a905fc8
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-12-08 13:48:46 +0100

    also track active users

--- statistics.sh
+++ statistics.sh
@@ -82,6 +82,7 @@ fi
 
 descriptions=(
     "Total number of installations"
+    "Total number of active installations"
     "Number of total requests and click on alternatives/ignores"
     "Number of clicks on an alternative"
     "Number of clicks on ignore"
@@ -92,6 +93,7 @@ date_dir="$data_dir/$DATE";
 
 cmds=(
     "ls $data_dir/installs | wc -l"
+    "ls $date_dir | wc -l"
     "find $date_dir -type f | wc -l"
     "ls $date_dir/ | grep -r '\"alternative\"' | wc -l"
     "ls $date_dir/ | grep -r '\"igore\"' | wc -l"
