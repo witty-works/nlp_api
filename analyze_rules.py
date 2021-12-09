abe1333c3772949428343ad69e8caed433390702
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-12-09 14:16:51 +0100

    expand cases we look for in the rule analysis

--- analyze_rules.py
+++ analyze_rules.py
@@ -47,6 +47,12 @@ def read_csv(in_file):
             if re.search("^.*[a-z]{3}innen~[^ ].*$", alternative):
                 print("Potential missing ~ in (innen~): " + alternative)
 
+            if re.search("^.*[a-z]e~.*$", alternative):
+                print("Potential missing ~ in (~e~): " + alternative)
+
+            if re.search("^.*e~r.*$", alternative):
+                print("Potential extra ~ in (er): " + alternative)
+
 
 @click.command()
 @click.option(
