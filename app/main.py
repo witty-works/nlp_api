13558c346c499d3b30c5deaa0a84679262530d5e
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-12-14 14:46:18 +0100

    removed unused import

--- app/main.py
+++ app/main.py
@@ -37,10 +37,7 @@ from fastapi import (
 
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
-
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-
-from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
