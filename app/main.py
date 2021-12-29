899521bf0317dfaf698ae76ce6088b75806ad7d1
Lukas Kahwe Smith <lukas.smith@witty.works>
2021-12-29 16:28:28 +0100

    bump version

--- app/main.py
+++ app/main.py
@@ -69,7 +69,7 @@ logging.debug("app started with settings: %s", settings)
 # convert string of list into list of the strings
 # project models
 
-version = "1.10.6"
+version = "1.11.0"
 app = FastAPI(
     title="Witty NLP API",
     version=version,
