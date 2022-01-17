04497681e7d16ef3e84c718332cba6202fa3dc05
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-01-17 15:35:10 +0100

    ensure we always have a language variant, fixes #296 NLP_API_25

--- app/main.py
+++ app/main.py
@@ -430,10 +430,14 @@ async def languagetool_rules(user_request_in: RequestIn):
                 lang = None
             elif "matches" in result:
                 locale = result["language"]["code"]
-                if lang == "en" and locale != "en-US":
+                if locale == "en":
+                    locale = "en-US"
+                elif locale == "de":
+                    locale = "de-DE"
+                elif lang == "en" and locale != "en-US":
                     locale = "en-GB"
 
-                lang = Lang(result["language"]["code"])
+                lang = Lang(locale)
                 german_gender_ending = user_request_in.config.german_gender_ending
 
                 if "orthography" not in user_request_in.config.disabled_categories:
