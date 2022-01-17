082267dc47a46d780a7cd702be78be239c926505
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-01-17 15:33:04 +0100

    strip newlines before tokenization, fixes #295 aka NLP_API_24

--- app/main.py
+++ app/main.py
@@ -480,7 +480,7 @@ async def languagetool_rules(user_request_in: RequestIn):
 
 def language_rules(user_request_in: RequestIn, lang: Lang):
     # apply SpaCy pre-built model
-    tokens = model[lang.lang](user_request_in.text.strip())
+    tokens = model[lang.lang](user_request_in.text.strip().replace("\n", " "))
 
     # functions for German rules
     if lang.lang == "de":
