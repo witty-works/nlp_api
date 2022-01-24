47d0f8605f6501b76536d89e5c4d6966b13ef2af
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-01-24 18:14:41 +0100

    bump version

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ from collections import namedtuple, defaultdict
 from collections import namedtuple
 from app.sentry import set_up_sentry_sdk
 
-version = "1.15.0"
+version = "1.15.1"
 
 settings = get_settings()
 logging = set_up_logger(settings)
