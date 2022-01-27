bb50283a27abf778fe482c1693dd695aece5cacb
Karolina Bozek <bozek.karolina@witty.works>
2022-01-27 10:46:31 +0100

    bump version to 1.16.0

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ from collections import namedtuple, defaultdict
 from collections import namedtuple
 from app.sentry import set_up_sentry_sdk
 
-version = "1.15.2"
+version = "1.16.0"
 
 settings = get_settings()
 logging = set_up_logger(settings)
