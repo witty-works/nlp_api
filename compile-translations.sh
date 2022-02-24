2147984582c7b0f1b22bbd1184ced7b7eaad3d59
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-02-24 13:03:44 +0100

    prevent translation of behavior subcategory into en-GB (#409)

--- compile-translations.sh
+++ compile-translations.sh
@@ -3,10 +3,23 @@
 mkdir -p locales/en_GB/LC_MESSAGES
 rm -rf locales/en_GB/LC_MESSAGES/*
 cp -r locales/en_US/LC_MESSAGES/messages.po locales/en_GB/LC_MESSAGES/.
+pipenv run python -m eng locales/en_GB/LC_MESSAGES/. --ext=po --target="uk"
+
+if [ "$(uname)" == "Darwin" ]; then
+    find 'locales/en_GB/LC_MESSAGES' -name '*.po' -print0 | xargs -0 sed -i '' 's/rules\.behaviour/rules.behavior/g'
+else
+    find 'locales/en_GB/LC_MESSAGES' -name '*.po' -print0 | xargs -0 sed -i 's/rules\.behaviour/rules.behavior/g'
+fi
 
 rm -rf training_data/en-GB/*
 cp training_data/en-US/* training_data/en-GB/.
 pipenv run python -m eng training_data/en-GB/. --ext=csv --target="uk"
+
+if [ "$(uname)" == "Darwin" ]; then
+    find 'training_data/en-GB' -name '*.csv' -print0 | xargs -0 sed -i '' 's/,behaviour,/,behavior,/g'
+else
+    find 'training_data/en-GB' -name '*.csv' -print0 | xargs -0 sed -i 's/,behaviour,/,behavior,/g'
+fi
 
 mkdir -p locales/de_AT/LC_MESSAGES
 rm -rf locales/de_AT/LC_MESSAGES/*
