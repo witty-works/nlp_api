a150d47f04beba2e9ca6c528bcc9191ddf78bf05
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-04-22 12:22:59 +0200

    filter out emails, urls and numbers in context and sentry

---
+++ app/privacy_filter.py
@@ -0,0 +1,104 @@
+import re
+from functools import lru_cache
+
+
+class PrivacyFilter:
+    """PrivacyFilter
+    Note:
+        * https://github.com/lmeulen/PrivacyFilter
+    """
+
+    def __init__(self):
+        # Make the URL regular expression
+        # https://stackoverflow.com/questions/827557/how-do-you-validate-a-url-with-a-regular-expression-in-python
+        ul = "\u00a1-\uffff"  # Unicode letters range (must not be a raw string).
+
+        # IP patterns
+        ipv4_re = r"(?:0|25[0-5]|2[0-4]\d|1\d?\d?|[1-9]\d?)(?:\.(?:0|25[0-5]|2[0-4]\d|1\d?\d?|[1-9]\d?)){3}"
+        ipv6_re = (
+            r"\[?((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,"
+            r"4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{"
+            r"1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2["
+            r"0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,"
+            r"3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|["
+            r"1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,"
+            r"2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|((["
+            r"0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2["
+            r"0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:["
+            r"0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2["
+            r"0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,"
+            r"5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\]?"
+        )
+
+        # Host patterns
+        hostname_re = (
+            r"[a-z" + ul + r"0-9](?:[a-z" + ul + r"0-9-]{0,61}[a-z" + ul + r"0-9])?"
+        )
+        # Max length for domain name labels is 63 characters per RFC 1034 sec. 3.1
+        domain_re = r"(?:\.(?!-)[a-z" + ul + r"0-9-]{1,63}(?<!-))*"
+        tld_re = (
+            r"\."  # dot
+            r"(?!-)"  # can't start with a dash
+            r"(?:[a-z" + ul + "-]{2,63}"  # domain label
+            r"|xn--[a-z0-9]{1,59})"  # or punycode label
+            r"(?<!-)"  # can't end with a dash
+            r"\.?"  # may have a trailing dot
+        )
+        host_re = "(" + hostname_re + domain_re + tld_re + "|localhost)"
+
+        self.url_re = re.compile(
+            r"([a-z0-9.+-]*:?//)?"  # scheme is validated separately
+            r"(?:[^\s:@/]+(?::[^\s:@/]*)?@)?"  # user:pass authentication
+            r"(?:" + ipv4_re + "|" + ipv6_re + "|" + host_re + ")"
+            r"(?::\d{2,5})?"  # port
+            r"(?:[/?#][^\s]*)?",  # resource path
+            re.IGNORECASE,
+        )
+
+    def remove_numbers(self, text):
+        return re.sub(r"\w*\d+\w*", "<NUMBER>", text).strip()
+
+    def remove_email(self, text):
+        return re.sub(
+            "(([a-zA-Z0-9_+]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?))"
+            "(?![^<]*>)",
+            "<EMAIL>",
+            text,
+        )
+
+    def remove_url(self, text):
+        text = re.sub(self.url_re, "<URL>", text)
+        return text
+
+    def filter_regular_expressions(self, text):
+        text = self.remove_email(text)
+        text = self.remove_url(text)
+        text = self.remove_numbers(text)
+        return text
+
+    def clean(self, text):
+        return self.filter_regular_expressions(text)
+
+    def clean_dict(self, dict):
+        for dict_key in dict.keys():
+            dict[dict_key] = self.clean_var(dict[dict_key])
+
+        return dict
+
+    def clean_var(self, var):
+        if isinstance(var, str):
+            var = self.clean(var)
+
+        if isinstance(var, dict):
+            var = self.clean_dict(var)
+
+        if isinstance(var, list):
+            for key in range(len(var)):
+                var[key] = self.clean_var(var[key])
+
+        return var
+
+
+@lru_cache()
+def get_privacy_filter():
+    return PrivacyFilter()
--- app/sentry.py
+++ app/sentry.py
@@ -1,5 +1,31 @@
 import sentry_sdk
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
+from app.privacy_filter import get_privacy_filter
+from app.privacy_filter import PrivacyFilter
+
+
+def sentry_clean_sensitive_frame(
+    frame, privacy_filter: PrivacyFilter
+):  # pragma: no cover
+    for var_name in frame.get("vars", None):
+        frame["vars"][var_name] = privacy_filter.clean_var(frame["vars"][var_name])
+
+    return frame
+
+
+def sentry_clean_event_data(event):  # pragma: no cover
+    privacy_filter = get_privacy_filter()
+
+    for exception in event.get("exception", {}).get("values", []):
+        for frame in exception.get("stacktrace", {}).get("frames", []):
+            frame = sentry_clean_sensitive_frame(frame, privacy_filter)
+
+    for exception in event.get("threads", {}).get("values", []):
+        for frame in exception.get("stacktrace", {}).get("frames", []):
+            frame = sentry_clean_sensitive_frame(frame, privacy_filter)
+
+    return event
+
 
 # Sentry SDK set up
 def set_up_sentry_sdk(version, settings):
@@ -15,6 +41,7 @@ def set_up_sentry_sdk(version, settings):
         integrations=integrations,
         release=version,
         environment=settings.platform_environment,
+        before_send=sentry_clean_event_data,
     )
 
     return sentry_sdk
