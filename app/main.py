cd1a926473956ad6a129ebae4df88471be7fdacc
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-04-23 12:40:44 +0200

    do not break if an unknown subcategory is used

--- app/main.py
+++ app/main.py
@@ -762,6 +762,15 @@ def type_transform(lemma_type):
 
 
 def is_sub_category_enabled(config: Config, subcategory: str):
+    if subcategory not in categories:
+        if not settings.is_prod:
+            logging.error(
+                "Subcategory is not defined: %s",
+                subcategory,
+            )
+
+        return False
+
     if categories[subcategory]["category"] in config.disabled_categories:
         return False
 
@@ -1864,7 +1873,6 @@ def rules_based_words_phrase_matcher_en(
 def homonyms_english(
     version: float, config: Config, lang, full_text, tokens, homonyms_words
 ):
-
     list_tokens = []
     for token in tokens:
 
