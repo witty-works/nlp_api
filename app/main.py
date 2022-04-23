ab5719c31b7fd421c3b402ac726831692b95e52b
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-04-23 08:52:52 +0200

    handle singular_they as bool

--- app/main.py
+++ app/main.py
@@ -66,7 +66,7 @@ from collections import defaultdict
 
 from app.sentry import set_up_sentry_sdk
 
-version = "1.26.2"
+version = "1.26.3"
 
 settings = get_settings()
 logging = set_up_logger(settings)
