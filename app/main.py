def6e239711db273b620f273e4de3cff2f6ae769
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-08-04 15:26:11 +0200

    bump version

--- app/main.py
+++ app/main.py
@@ -71,7 +71,7 @@ from collections import defaultdict
 
 from app.sentry import set_up_sentry_sdk
 
-version = "1.33.1"
+version = "1.34.0"
 
 settings = get_settings()
 logging = set_up_logger(settings)
