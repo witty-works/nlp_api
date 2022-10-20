ce50c0a05ebb5e67104c8b6e98eb57b87ade37ec
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-10-20 08:53:49 +0200

    bump version

--- app/main.py
+++ app/main.py
@@ -76,7 +76,7 @@ from app.model import model
 from app.rules import *
 from app.sentry import set_up_sentry_sdk
 
-version = "1.38.0"
+version = "1.38.1"
 
 settings = get_settings()
 logging = set_up_logger(settings)
