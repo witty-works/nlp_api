56115fceaddfcc6fcd10fabf9884c8b2affcabcf
Lukas Kahwe Smith <lukas.smith@witty.works>
2022-11-24 09:58:04 +0100

    fix regresson with 'good at'

--- app/rules.py
+++ app/rules.py
@@ -749,7 +749,7 @@ list_false_column = [
     "push the boundaries",
     "fossil promo",
     "good friends",
-    "good at"
+    "good at",
     "of a kind",
     "great at",
     "Minority Ethnic",
