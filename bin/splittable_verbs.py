f16212781f55703e53e5c7cb8f20ac1f328f10e3
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-01-21 15:55:53 +0100

    handle 'ge' inside verbs for grammatical correctness

---
+++ bin/splittable_verbs.py
@@ -0,0 +1,31 @@
+import csv
+
+from app.main import german_verb_splittable
+from app.models import Language
+
+# csv header
+fieldnames = ["name", "area", "country_code2", "country_code3"]
+
+lang = Language("de-DE")
+
+with open("verbs.csv", "w", encoding="UTF8", newline="") as f:
+    with open("./training_data/de/verbs.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+
+                fieldnames = row
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()
+            elif line_count == 1:
+                prefix = german_verb_splittable(row[0], lang)
+                if prefix == False:
+                    prefix = ""
+
+                row[-1] = prefix
+
+                row_dict = {fieldnames[i]: row[i] for i in range(len(fieldnames))}
+
+                writer.writerow(row_dict)
