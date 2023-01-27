af4efecc004f58000c7c6801dff4845c14c375df
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-01-27 22:24:21 +0100

    handle alternative_context in alternative_context()
    fixes #734

--- app/main.py
+++ app/main.py
@@ -2545,8 +2545,21 @@ def align_verb_form(lang, a_text, a_token, b_token):
 
 
 def alternative_declension(lang, text, token, word_types, alternative):
-    if alternative == "-" or ResultOut.isInspirationAlternative(text, alternative):
-        return alternative
+    if "---" in alternative:
+        (
+            alternative,
+            alternative_context,
+        ) = ResultOut.parse_alternative_context(alternative)
+        alternative_context = " ---" + alternative_context
+    else:
+        alternative_context = ""
+
+    if (
+        alternative == ""
+        or alternative[0] == "-"
+        or ResultOut.isInspirationAlternative(text, alternative)
+    ):
+        return alternative + alternative_context
 
     new_alternative = ""
     previous = False
@@ -2587,7 +2600,7 @@ def alternative_declension(lang, text, token, word_types, alternative):
             alternative_text + alternative_token.whitespace_ + new_alternative
         )
 
-    return new_alternative
+    return new_alternative + alternative_context
 
 
 def alternatives_declension(lang, token, alternatives, prev_token):
