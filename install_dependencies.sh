1b36935589a3751b5be374c4ee93321f0b56f61d
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-03-10 14:35:49 +0100

    install depedencies in all container

---
+++ install_dependencies.sh
@@ -0,0 +1,22 @@
+du -sh $PLATFORM_CACHE_DIR
+mkdir -p $HOME/.global
+mkdir -p $HOME/.venv
+mkdir -p $PLATFORM_CACHE_DIR/.global 
+mkdir -p $PLATFORM_CACHE_DIR/.venv
+
+echo "Restoring from  build cache"
+rsync -ar $PLATFORM_CACHE_DIR/.venv/ $HOME/.venv
+rsync -ar $PLATFORM_CACHE_DIR/.global/ $HOME/.global
+echo "Done restoring from build cache"
+
+echo "Installing pdm and pdm dependecies"
+pip install pdm
+pdm config install.cache True
+pdm config cache_dir $PLATFORM_CACHE_DIR/pdm
+pdm run python -m ensurepip
+pdm install
+
+echo "Saving to build cache"
+rsync -ar $HOME/.venv/ $PLATFORM_CACHE_DIR/.venv
+rsync -ar $HOME/.global/ $PLATFORM_CACHE_DIR/.global
+echo "Done saving to build cache"
