0d4fb0d9b67f974acbc4e136644d3b818eb4f78e
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-10-12 08:55:19 +0200

    fix pydantic deprection warnings

--- app/main.py
+++ app/main.py
@@ -840,7 +840,7 @@ async def post_organization_configs(
     organization_configs: OrganizationConfRequest,
     username: str = Depends(fetch_current_username),
 ):
-    redis.set(organization_configs.id, organization_configs.json())
+    redis.set(organization_configs.id, organization_configs.model_dump_json())
 
     return organization_configs
 
@@ -877,7 +877,7 @@ async def get_organization_configs(
 async def post_user_configs(
     user_configs: UserConfRequest, username: str = Depends(fetch_current_username)
 ):
-    redis.set(user_configs.email.lower(), user_configs.json())
+    redis.set(user_configs.email.lower(), user_configs.model_dump_json())
 
     return user_configs
 
@@ -1303,14 +1303,14 @@ def fetch_result_conf(configs: dict):
         return None
 
     organization_config = (
-        RuleConfig.parse_obj(configs["organization_config"])
+        RuleConfig.model_validate(configs["organization_config"])
         if "organization_config" in configs
         else None
     )
 
     plan = configs["plan"]
 
-    config = RuleConfig.parse_obj(configs["config"])
+    config = RuleConfig.model_validate(configs["config"])
 
     return ResultConf(
         id=configs["id"],
