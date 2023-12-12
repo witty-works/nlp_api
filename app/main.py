5c718379e1348379882171fe8b8841c57f2242fd
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-12-12 13:25:12 +0100

    drop async-lru in favor of redis
    hoping this can solve instability https://support.platform.sh/hc/en-us/requests/270894\?page\=1

--- app/main.py
+++ app/main.py
@@ -1372,16 +1372,18 @@ async def fetch_user(request: Request) -> str | None:
 
                 if "domain" in config:
                     claims = await decode_b2c_jwt(
+                        redis,
                         ssl_session,
                         request,
                         config["tenant_id"],
                         config["client_id"],
+                        config["expected_scope"],
                         config["domain"],
                         config["policy"],
-                        config["expected_scope"],
                     )
                 else:
                     claims = await decode_jwt(
+                        redis,
                         ssl_session,
                         request,
                         config["tenant_id"],
