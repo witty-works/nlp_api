61310f0b1b7029e65fb9c66460f59b0c6f375905
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-12-15 08:34:54 +0100

    add max requests to prevent OOM kills

--- app/main.py
+++ app/main.py
@@ -102,7 +102,7 @@ from app.model import fetch_nlp_model
 from app.rules import fetch_static_rules
 from app.sentry import set_up_sentry_sdk
 
-version = "1.50.2"
+version = "1.50.3"
 
 categories = get_categories()
 settings = get_settings()
