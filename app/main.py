f1036e7ec75fc4899fa4d3cbe4592550e7f88ef8
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-02-12 09:25:30 +0100

    fix fetching alternatives for child rules

--- app/main.py
+++ app/main.py
@@ -2361,7 +2361,7 @@ async def fetch_rule_alternatives(
 
     query = f"SELECT {alternative_column_list} FROM rules_alternative WHERE rule_id = ?"
 
-    parameters = [rule.id]
+    parameters = [rule.parent_id if rule.parent_id else rule.id]
 
     if not show_inspiration_alternatives:
         query += " and is_inspiration = 0"
