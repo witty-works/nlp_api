e9c71b4bf981f6fa02064b508b4292c5cc8c7278
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-03-14 16:13:37 +0100

    force_categories is optional

--- app/models.py
+++ app/models.py
@@ -435,7 +435,7 @@ class RuleConfig(BaseModel):
     german_gender_ending: Optional[GermanGenderEndingConfigType] = None
     gendered_roles_format: Optional[GenderedRolesFormatConfigType] = None
     categories: dict[str, BooleanConfigType] = {}
-    force_categories: list[str] = []
+    force_categories: Optional[list[str]] = []
     show_inspiration_alternatives: Optional[BooleanConfigType] = None
 
     @field_validator("preferred_variants", mode="before")
