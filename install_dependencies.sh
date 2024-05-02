33d397561c0cf54dce6d51aa34cb58593f445e44
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-05-02 11:41:23 +0200

    speed up deployment in dev by either adding french or using pdm sync --prod

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,4 +3,13 @@ pip install pdm
 pdm run python -m ensurepip
 
 echo "Installing dependencies using pdm"
-pdm sync --prod
+
+if [ -z "${MODELS##*"fr_core_news_sm"*}" ]; then
+    echo "Installing French fr_core_news_sm model"
+    pdm add "fr_core_news_sm @ https://github.com/explosion/spacy-models/releases/download/fr_core_news_sm-3.7.0/fr_core_news_sm-3.7.0-py3-none-any.whl"
+elif [ -z "${MODELS##*"fr_core_news_lg"*}" ]; then
+    echo "Installing French fr_core_news_lg model"
+    pdm add "fr_core_news_lg @ https://github.com/explosion/spacy-models/releases/download/fr_core_news_lg-3.7.0/fr_core_news_lg-3.7.0-py3-none-any.whl"
+else
+    pdm sync --prod
+fi
