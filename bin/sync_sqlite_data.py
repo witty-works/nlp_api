e268f3b8c4e7d35c878a8bcc62c09b748a1f3f51
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-09-16 16:16:08 +0200

    export french lemmas as well

--- bin/sync_sqlite_data.py
+++ bin/sync_sqlite_data.py
@@ -61,7 +61,7 @@ source.execute("DELETE FROM rules_rule WHERE is_active = 0")
 
 lookup = {}
 lemma_plural_lookup = {}
-langs = [LangType.EN, LangType.DE]
+langs = [LangType.EN, LangType.DE, LangType.FR]
 for lang in langs:
     query = "SELECT text, lemma, is_plural FROM rules_lemmatization WHERE language = ?"
     parameters = [lang]
