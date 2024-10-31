6f3765ed82d55b1c36efda56ee6ec749a571b28d
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-10-31 15:13:35 +0100

    bump version

--- app/context.py
+++ app/context.py
@@ -36,7 +36,7 @@ from app.http import Http
 
 
 class AppContext:
-    version = "2.3.9"
+    version = "2.3.10"
     translations: dict[str, dict[str, str]]
     declensions_config: dict
     verb_form_map: dict
