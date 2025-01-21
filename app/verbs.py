5895f82521b6c889aaed1592624bfe490a1779f9
Lukas Kahwe Smith <lukas.smith@witty.works>
2025-01-21 08:52:11 +0100

    fix german_verb_splittable()

--- app/verbs.py
+++ app/verbs.py
@@ -79,9 +79,9 @@ class Verbs:
             if word.startswith(prefix):
                 return prefix
 
-        for prefix in static_rules[LangType.DE]["splittable_words"]:
+        for prefix in self.static_rules[LangType.DE]["splittable_words"]:
             if word.startswith(prefix):
-                if word in static_rules[LangType.DE]["splittable_words"][prefix]:
+                if word in self.static_rules[LangType.DE]["splittable_words"][prefix]:
                     return prefix
 
                 return None
